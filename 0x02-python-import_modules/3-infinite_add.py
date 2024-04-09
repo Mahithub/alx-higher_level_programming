d7ace056219e8d4997f15f49a0c84fdd88d1fce2
Mahithub <recappingmovie@gmail.com>
2024-04-09 13:55:43 -0700

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    """program that prints the result of the addition of all arguments"""
+
+    total = 0
+    
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+
