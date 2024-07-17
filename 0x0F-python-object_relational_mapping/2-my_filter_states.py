db1d547ac6815096272c88c0058766e7903905ad
Mahithub <recappingmovie@gmail.com>
2024-07-17 03:08:40 -0700

    commited

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,14 +2,15 @@
 # Displays all values in the states table of hbtn_0e_0_usa.
 # Usage: ./2-my_filter_states.py <mysql username> \
 #                                <mysql password> \
-#                                <database name>
-
+#                                <database name> \
+#                                <state name searched>
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.excute("SELECT * FROM 'states' WHERE BINARY \
-            'name = {}'".format(sys.argv[4]))
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
     [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,6 +9,9 @@
 import sys
 import MySQLdb
 
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
